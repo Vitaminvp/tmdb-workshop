4f71d7d5de5d34fc9e27c3213060e57676f7fde4
Ivan Tytarenko <ivant@wix.com>
2019-06-01 12:32:08 +0300

    adds data fetching

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import logo from './logo.svg';
 import './App.css';
 
-import { getMovies } from './api/api';
+import { getMovies } from './redux/movies';
 
-getMovies().then(console.log);
+const mapStateToProps = state => ({
+  movies: state.movies.list,
+  isLoading: state.movies.isLoading,
+});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapDispatchToProps = {
+  getMovies,
+};
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.getMovies();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/redux/movies.js
+++ src/redux/movies.js
@@ -1,8 +1,38 @@
+import * as api from '../api/api';
+
+const FETCH_MOVIES = 'FETCH_MOVIES';
+const FETCH_MOVIES_ERROR = 'FETCH_MOVIES_ERROR';
+const FETCH_MOVIES_COMPLETED = 'FETCH_MOVIES_COMPLETED';
+
 const initialState = {
   movies: [],
   isLoading: false,
 };
 
+const fetchMovies = () => ({ type: FETCH_MOVIES });
+
+const fetchMoviesError = error => ({
+  type: FETCH_MOVIES_ERROR,
+  payload: { error },
+});
+
+const fetchMoviesCompleted = movies => ({
+  type: FETCH_MOVIES_COMPLETED,
+  payload: { movies },
+});
+
 export default (state = initialState, action) => {
   return state;
+};
+
+export const getMovies = () => async dispatch => {
+  dispatch(fetchMovies());
+
+  try {
+    const movies = await api.getMovies();
+
+    dispatch(fetchMoviesCompleted(movies));
+  } catch (e) {
+    dispatch(fetchMoviesError(e));
+  }
 };
